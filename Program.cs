cfdf7cfa56fbaaf721cb3d106f133641ecd302dd
DESKTOP-C5ELEEH\eniac <eniacgabriel@gmail.com>
2022-03-23 23:46:14 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaACME
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuario usuarioBob = new Usuario("Bob Nelson", 37, 1.70F, 78F); 
+            Usuario usuarioTestolfo = new Usuario("Testolfo", 43, 1.65F, 60F);
+            Usuario usuarioLisa = new Usuario("Lisa Leite", 22, 1.72F, 92F);
+
+            Console.WriteLine("Bob");
+            usuarioBob.DescricaoIMC();
+            Console.ReadLine();
+
+            Console.WriteLine("Testolfo");
+            usuarioTestolfo.DescricaoIMC();
+            Console.ReadLine();
+
+            Console.WriteLine("Lisa");
+            usuarioLisa.DescricaoIMC();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Usuario.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace SistemaACME
+{
+    class Usuario
+    {
+        public String nome;
+        public int idade;
+        public float altura;
+        public float peso;
+
+
+        public Usuario(string nome, int idade, float altura, float peso)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.altura = altura;
+            this.peso = peso;
+        }
+
+        public float CalcularImc()
+        {
+            return peso / (altura)
+        }
+
+        /*
+        public float CalcularMeta()
+        {
+           
+            float meta = peso - imc;
+            return meta;
+        }
+        */
+
+        public void DescricaoIMC()
+        {
+            //Calcular IMC do Usuário
+            float imc = CalcularImc();
+            
+
+            float meta = peso-imc;
+            Math.Sqrt(meta);
+
+
+            Console.WriteLine($"Seu IMC atual é {imc}.");
+
+            //Condição
+            if (imc >= 18.2 & imc <= 24.9)
+            {
+                Console.WriteLine("Parabens você está com seu peso normal");
+            }
+            if (imc >= 25.0 & imc <= 29.9)
+            {
+                Console.WriteLine("Você está acima do seu peso.");
+                Console.WriteLine($"META: Você precisa emagrecer{meta}");
+            }
+            if (imc >= 30.0 & imc <= 34.9)
+            {
+                Console.WriteLine("Obesidade Grau 1");
+                Console.WriteLine($"META: Você precisa emagrecer{meta}");
+            }
+            if (imc >= 35.0 & imc <= 40)
+            {
+                Console.WriteLine("Obesidade Grau 2");
+                Console.WriteLine($"META: Você precisa emagrecer{meta}");
+            }
+            if (imc >= 40.0)
+            {
+                Console.WriteLine("Obesidade Grau 3 e 4.");
+                Console.WriteLine($"META: Você precisa emagrecer{meta}");
+            }
+        }
+    }
+}
